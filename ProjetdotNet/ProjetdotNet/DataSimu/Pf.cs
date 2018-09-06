4d87488c02361bd4da94c60ffb0716d11008fdf3
qadadiaImad <imad.qadadia@gmail.com>
2018-09-06 11:02:25 +0200

    pf.cs squelette

--- ProjetdotNet/ProjetdotNet/DataSimu/Pf.cs
+++ ProjetdotNet/ProjetdotNet/DataSimu/Pf.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PricingLibrary.Utilities.MarketDataFeed;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,28 @@ namespace ProjetdotNet.DataSimu
 {
     class Pf
     {
-        
+        Dictionary<string, decimal> Composition;
+
+        decimal cash_sr;
+
+        DateTime CashInvestementDate;
+
+        public void UpdateVal(DataFeed df)
+        {
+
+
+        }
+
+        public void InvestCash(DateTime time,decimal cash)
+        {
+
+            
+        }
+
+        public void UpdateCompo(Dictionary<string, decimal> Composition)
+        {
+
+
+        }
     }
 }
